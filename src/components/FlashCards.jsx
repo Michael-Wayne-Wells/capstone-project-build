2e3257d78f8fbd8ba4ac54ed5ff8c6c0abb01515
Michael Wells <mwells1286@gmail.com>
2020-03-06 10:34:59 -0800

    build initial file structure and components

---
+++ src/components/FlashCards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FlashCards() {
+  return <div>FlashCards</div>;
+}
+
+export default FlashCards;
---
+++ src/components/Vulnerabilities.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Vulnerabilities() {
+  return <div>Vulnerabilities</div>;
+}
+
+export default Vulnerabilities;
