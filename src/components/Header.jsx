67955e14eb6e887688954afbe0997095be48d143
Michael Wells <mwells1286@gmail.com>
2020-03-07 17:57:17 -0800

    update pages for new packages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { FaUsers } from "react-icons/fa";
 
-function Header() {
-  return <div>Header</div>;
+class Header extends React.Component {
+  render() {
+    const { user } = this.props;
+
+    return (
+      <nav className="site-nav family-sans navbar navbar-expand bg-primary navbar-dark higher">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">
+            <img style={{ height: "25px" }} src="goose.png" alt="goose" /> OWASP
+            trainer
+          </a>
+          <div className="navbar-nav ml-auto">
+            <a className="nav-item nav-link" href="/learn">
+              Learn the OWASP top 10
+            </a>
+
+            {!user && (
+              <a className="nav-item nav-link" href="/login">
+                log in
+              </a>
+            )}
+            {!user && (
+              <a className="nav-item nav-link" href="/register">
+                register
+              </a>
+            )}
+            {user && (
+              <a className="nav-item nav-link" href="/login">
+                log out
+              </a>
+            )}
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
--- src/components/Vulnerabilities.jsx
+++ src/components/Vulnerabilities.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Quiz from "./Quiz";
+
 function Vulnerabilities() {
   return (
     <div>
