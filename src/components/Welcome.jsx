72f0a55b9b0b632839cfede9adf03b4358cd9765
Michael Wells <mwells1286@gmail.com>
2020-03-08 14:53:37 -0700

    add blob

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Blob from "./Blob";
 
 function Welcome(props) {
   const user = props.user;
-  return <div>Welcome {user} </div>;
+  return (
+    <div>
+      <h2>Welcome {user} </h2>
+      <Blob />
+    </div>
+  );
 }
 
 export default Welcome;
