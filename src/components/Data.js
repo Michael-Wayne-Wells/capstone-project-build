412ff7115931719bca46b1f60f892d0daed068e7
Michael Wells <mwells1286@gmail.com>
2020-03-09 09:17:14 -0700

    add paralex scrolling in vulns

---
+++ src/components/Data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: "Injection",
+    rank: 1
+  },
+  {
+    name: "Broken Authentication & Session Management",
+    rank: 2
+  },
+  {
+    name: "Cross-Site Scripting (XSS))",
+    rank: 3
+  },
+  {
+    name: "Insecure Direct Object References",
+    rank: 4
+  },
+  {
+    name: "Security Misconfiguration",
+    rank: 5
+  },
+  {
+    name: "Sensitive Data Exposure",
+    rank: 6
+  },
+  {
+    name: "Missing Function Level Access Control",
+    rank: 7
+  },
+  {
+    name: "Cross-Site Request Forgery (CSRF)",
+    rank: 8
+  },
+  {
+    name: "Using Component with Known Vulnerabilities",
+    rank: 9
+  },
+  {
+    name: "Unvalidated Redirects and Forwards",
+    rank: 10
+  }
+];
--- src/components/Vulnerabilities.jsx
+++ src/components/Vulnerabilities.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Quiz from "./Quiz";
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
+import PropTypes from "prop-types";
+class Vulnerabilities extends React.Component {
+  render() {
+    return (
+      <Parallax ref={ref => (this.parallex = ref)} pages={3}>
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <h2>{this.props.rank}</h2>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={1}
+          speed={0.1}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <h2>{this.props.name}</h2>
+        </ParallaxLayer>
 
-function Vulnerabilities() {
-  return (
-    <div>
-      Vulnerabilities
-      <Quiz />
-    </div>
-  );
+        <ParallaxLayer
+          offset={2}
+          speed={-0}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        ></ParallaxLayer>
+      </Parallax>
+    );
+  }
 }
-
+Vulnerabilities.propTypes = {
+  name: PropTypes.string,
+  rank: PropTypes.number
+};
 export default Vulnerabilities;
